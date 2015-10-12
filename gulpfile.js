643cf724778295643ddaf5bddfa7728a56867e48
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-12 00:20:19 -0700

    Replaced webserver with connect
    The connect task seems to be much more popular, and it actually worked
    out of the box unlike the other live-reload package.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 
-var server = require('gulp-server-livereload');
-
-gulp.task('webserver', function() {
-  gulp.src('./app/')
-    .pipe(server({
-      host: '0.0.0.0',
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    host: '0.0.0.0'
+  });
 });
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
