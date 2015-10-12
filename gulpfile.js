e300ac5a52b4745956d6ebacb69ac2ba96f86e28
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-12 00:04:31 -0700

    Now serve over the entire network

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 
 gulp.task('webserver', function() {
-  gulp.src('app')
+  gulp.src('./app/')
     .pipe(server({
+      host: '0.0.0.0',
       livereload: true,
       directoryListing: true,
       open: true
