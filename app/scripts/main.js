a2e70134a908d763c37f0e99e0538e9ff78bd69f
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-19 02:16:05 -0700

    Authorization now works using Knockout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,74 +5,6 @@ $(document).ready(function() {
 
   var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 
-  /**
-    * Check if current user has authorized this application.
-    */
-  function checkAuth() {
-    gapi.auth.authorize(
-      {
-        'client_id': CLIENT_ID,
-        'scope': SCOPES.join(' '),
-        'immediate': true
-      }, handleAuthResult);
-  }
-
-  /**
-    * Handle response from authorization server.
-    *
-    * @param {Object} authResult Authorization result.
-    */
-  function handleAuthResult(authResult) {
-    var authorizeDiv = document.getElementById('auth');
-    if (authResult && !authResult.error) {
-      // Hide auth UI, then load client library.
-      authorizeDiv.style.display = 'none';
-      loadDriveApi();
-    } else {
-      // Show auth UI, allowing the user to initiate authorization by
-      // clicking authorize button.
-      authorizeDiv.style.display = 'inline';
-    }
-  }
-
-  /**
-    * Initiate auth flow in response to user clicking authorize button.
-    *
-    * @param {Event} event Button click event.
-    */
-  function handleAuthClick(event) {
-    console.log('Authorizing...');
-    gapi.auth.authorize(
-      {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-      handleAuthResult);
-    return false;
-  }
-
-  /**
-    * Load Drive API client library.
-    */
-  function loadDriveApi() {
-    gapi.client.load('drive', 'v2', listFiles);
-  }
-
-  /**
-    * Print files.
-    */
-  function listFiles() {
-    var request = gapi.client.drive.files.list({
-        'maxResults': 30
-      });
-
-    var filesSection = $('#files');
-
-    request.execute(function(resp) {
-      var files = resp.items;
-
-      files.forEach(function(file) {
-        appendFile(file, filesSection);
-      });
-    });
-  }
 
   /**
     * Adds a new element into our files section
@@ -96,9 +28,94 @@ $(document).ready(function() {
       .append(title)
       .append(description);
 
-    fileRef.append(fileContainer);
   }
 
-  $('#authorize-button').click(handleAuthClick);
 
+  // Authorization model
+  function AuthModel() {
+    var self = this;
+
+    /**
+      * Check if current user has authorized this application.
+      */
+    function checkAuth() {
+      gapi.auth.authorize(
+        {
+          'client_id': CLIENT_ID,
+          'scope': SCOPES.join(' '),
+          'immediate': true
+        }, handleAuthResult);
+    }
+
+    /**
+      * Handle response from authorization server.
+      *
+      * @param {Object} authResult Authorization result.
+      */
+    function handleAuthResult(authResult) {
+      var authorizeDiv = document.getElementById('auth');
+      if (authResult && !authResult.error) {
+        // Tell our model that everthing is authorized
+        self.isAuthorized(true);
+        loadDriveApi();
+      } else {
+        // Show auth UI, allowing the user to initiate authorization by
+        // clicking authorize button.
+        authorizeDiv.style.display = 'inline';
+      }
+    }
+
+    /**
+      * Initiate auth flow in response to user clicking authorize button.
+      *
+      * @param {Event} event Button click event.
+      */
+    function handleAuthClick() {
+      console.log('Authorizing...');
+      console.log(self);
+      gapi.auth.authorize(
+        {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+        handleAuthResult);
+      return false;
+    }
+
+    /**
+      * Load Drive API client library.
+      */
+    function loadDriveApi() {
+      gapi.client.load('drive', 'v2', listFiles);
+    }
+
+    /**
+      * Print files.
+      */
+    function listFiles() {
+      var request = gapi.client.drive.files.list({
+          'maxResults': 30
+        });
+
+      var filesSection = $('#files');
+
+      request.execute(function(resp) {
+        var files = resp.items;
+
+        files.forEach(function(file) {
+          appendFile(file, filesSection);
+        });
+      });
+    }
+
+    this.isAuthorized = ko.observable(false);
+    this.handleAuth = handleAuthClick();
+  }
+
+  // File Model
+  function FileModel() {
+    var self = this;
+
+
+  }
+
+  ko.applyBindings(new AuthModel());
+  // ko.applyBindings(new FileModel());
 });
