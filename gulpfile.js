2f1eede5c130ac414f25453d9df2b2afa1bd5d3e
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-19 01:07:01 -0700

    Better js hinting

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
 
 gulp.task('connect', function() {
   connect.server({
@@ -16,6 +17,14 @@ gulp.task('html', function () {
 
 gulp.task('watch', function () {
   gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/scripts/*.js'], ['lint']);
+});
+
+gulp.task('lint', function() {
+  gulp.src('./app/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(connect.reload());
 });
 
 gulp.task('default', ['connect', 'watch']);
