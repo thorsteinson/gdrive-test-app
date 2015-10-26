60d8a948d5bd35b69ddab49d54729481320a5901
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-25 23:40:51 -0700

    Upload of XooML Works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -201,6 +201,62 @@ $(window).load(function() {
         self.navigateFolder({id: self.currentNav()});
       }
     }
+
+    // Writes a file in the current folder
+    this.write = function() {
+      const mimeType = 'text/xml';
+      function insertFile(fileData, callback) {
+        const boundary = '-------314159265358979323846';
+        const delimiter = "\r\n--" + boundary + "\r\n";
+        const close_delim = "\r\n--" + boundary + "--";
+
+        var reader = new FileReader();
+        reader.readAsBinaryString(fileData);
+        reader.onload = function(e) {
+          var contentType = fileData.type || 'application/octet-stream';
+          var metadata = {
+            'title': 'XooML2.xml',
+            'mimeType': contentType
+          };
+
+          var base64Data = btoa(reader.result);
+          var multipartRequestBody =
+              delimiter +
+              'Content-Type: application/json\r\n\r\n' +
+              JSON.stringify(metadata) +
+              delimiter +
+              'Content-Type: ' + contentType + '\r\n' +
+              'Content-Transfer-Encoding: base64\r\n' +
+              '\r\n' +
+              base64Data +
+              close_delim;
+
+          var request = gapi.client.request({
+              'path': '/upload/drive/v2/files',
+              'method': 'POST',
+              'params': {'uploadType': 'multipart'},
+              'headers': {
+                'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+              },
+              'body': multipartRequestBody});
+          if (!callback) {
+            callback = function(file) {
+              console.log(file)
+            };
+          }
+          request.execute(callback);
+        }
+      }
+
+      // in our case, replace with XooML
+      var xmlContentExample = "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>"
+
+      var blob = new Blob([xmlContentExample], {type: 'text/xml', fileName: 'XooML.xml'});
+      insertFile(blob, function() {
+        console.log('Finished file upload?');
+      });
+
+    }
   }
 
   // Applies our models to the DOM
